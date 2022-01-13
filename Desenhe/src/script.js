9b4270ddd08deb92b83450672a5de7a3b5cc787d
ThiagoMaylon <95234678+ThiagoMaylon@users.noreply.github.com>
2022-01-13 14:39:49 -0300

    consertando alguns bugs

--- Desenhe/src/script.js
+++ Desenhe/src/script.js
@@ -2,13 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
     let tela = document.querySelector("#tela");
     let ctx = tela.getContext('2d');
 
-    tela.width = 800;
-    tela.height = 390;
+    tela.width = 1200;
+    tela.height = 500;
     ctx.lineWidth = 2; 
     ctx.strokeStyle = "#00d9ff";
 
     let pincel = {
-        actie: false, 
+        ative: false, 
         movendo: false,
         pos: {x: 0, y: 0},
         posAnte: null,
@@ -22,21 +22,22 @@ document.addEventListener("DOMContentLoaded", () => {
         ctx.stroke();
     }
 
-    tela.onmousedown = () => {pincel.actie = true};
-    tela.onmouseup = () => {pincel.actie = false};
+    tela.onmousedown = () => {pincel.ative = true};
+    tela.onmouseup = () => {pincel.ative = false};
+    tela.onmouseout = () => {pincel.ative = false};
 
     tela.onmousemove = (evento) => {
-        pincel.pos.x = evento.clientX;
-        pincel.pos.y = evento.clientY;
+        pincel.pos.x = evento.clientX-tela.offsetLeft+scrollX;  //tela.
+        pincel.pos.y = evento.clientY-tela.offsetTop+scrollY; //
         pincel.movendo = true; 
     };
 
     let ciclo = () => {
-        if(pincel.actie && pincel.movendo && pincel.posAnte){
+        if(pincel.ative && pincel.movendo && pincel.posAnte){
             drawLine({pos: pincel.pos, posAnte: pincel.posAnte});
             pincel.movendo = false;
         }
-        pincel.posAnte = {x: pincel.pos.x, y: pincel.pos.y}
+        pincel.posAnte = {...pincel.pos}
 
         setTimeout(ciclo, 10)
     }
